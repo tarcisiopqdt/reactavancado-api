b0ec752fd72210cef21b989ad5d45751eb3fea4c
Paulo Tarcisio <paulo_tarcisio93@hotmail.com>
2020-10-30 17:14:35 -0300

    iniciando build strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '65bed7519931fc09e1f450f925ea5c51'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac9d483c-318e-417f-bac6-67c4f3a2c6dd'
+};
